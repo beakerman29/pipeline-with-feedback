95ae05e67c4afc2b8a8d26fe79c50cb68e7f5ab1
matthew <matthew.ramsey@live.com>
2020-03-26 13:10:42 -0700

    Add the hostname script for returning stuff

---
+++ Hostname.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import logging
+import argparse
+
+
+logging.basicConfig()
+LOGGER = logging.getLogger()
+LOGGER.setLevel('DEBUG')
+
+def main():
+    	""" The main code for this project
+		"""
+		parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+		parser.add_argument("--atestinput", help=' test input parameter to allow for seeing what happens if it is not set', choices=['set'], required= =True)
+		args = parser.parse_args()
+    	LOGGER.info('We are going to return a test value')
+		print("DummyHost3")
+
+
+if __name__ == '__main__':
+    	main()
