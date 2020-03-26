c12c806547ab6b34d2785dfbe5e1b72f0633dd03
matthew <matthew.ramsey@live.com>
2020-03-26 13:44:29 -0700

    Debuged the syntax and have script running

--- Hostname.py
+++ Hostname.py
@@ -8,13 +8,13 @@ LOGGER = logging.getLogger()
 LOGGER.setLevel('DEBUG')
 
 def main():
-    	""" The main code for this project
-		"""
-		parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-		parser.add_argument("--atestinput", help=' test input parameter to allow for seeing what happens if it is not set', choices=['set'], required= =True)
-		args = parser.parse_args()
-    	LOGGER.info('We are going to return a test value')
-		print("DummyHost3")
+	""" The main code for this project
+	"""
+	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument("--atestinput", help=' test input parameter to allow for seeing what happens if it is not set', choices=['set'], required=True)
+	args = parser.parse_args()
+	LOGGER.info('We are going to return a test value')
+	print("DummyHost3")
 
 
 if __name__ == '__main__':
